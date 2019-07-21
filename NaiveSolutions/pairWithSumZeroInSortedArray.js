f91ad6b0e4ca6285dabeceb95ebad6bcd55fadcb
mvs24 <40918003+mvs24@users.noreply.github.com>
2019-07-21 13:31:16 +0200

    Add files via upload

---
+++ NaiveSolutions/pairWithSumZeroInSortedArray.js
@@ -0,0 +1,14 @@
+function pairWithSumZeroInSortedArray(array) {
+    for (let i = 0; i < array.length - 1; i++) {
+      for (let j = i + 1; j < array.length; j++) {
+        if (array[i] + array[j] === 0) {
+          return [array[i], array[j]];
+        }
+      }
+    }
+  }
+  
+  console.log(
+      pairWithSumZeroInSortedArray([-4, -3, -2, -1, 0, 1, 2, 5])
+  );
+  
