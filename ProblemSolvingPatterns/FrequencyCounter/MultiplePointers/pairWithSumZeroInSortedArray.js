21d327e3fa369c737917934934b0415c83c28b51
mvs24 <40918003+mvs24@users.noreply.github.com>
2019-07-21 13:32:45 +0200

    Update pairWithSumZeroInSortedArray.js

--- ProblemSolvingPatterns/FrequencyCounter/MultiplePointers/pairWithSumZeroInSortedArray.js
+++ ProblemSolvingPatterns/FrequencyCounter/MultiplePointers/pairWithSumZeroInSortedArray.js
@@ -2,18 +2,15 @@ function pairWithSumZeroInSortedArray(array) {
   let result = [];
   let i = 0;
   let j = array.length - 1;
-  while(j >= i){
+  while(j > i){
     if(array[i] + array[j] > 0) {
         j--;
     }else if(array[i] + array[j] < 0) {
         i++;
     }else{
-        result.push(array[i]);
-        result.push(array[j]);
+        return[array[i],array[j]];
     }
   }
-  
-  return ( result.length === 2 ) ? result : undefined;
 }
 
-console.log(pairWithSumZeroInSortedArray([-3, -2, -1, 0, 1, 2, 4]));
+//console.log(pairWithSumZeroInSortedArray([-3, -2, -1, 0, 1, 2, 4]));
